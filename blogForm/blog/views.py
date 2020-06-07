8d9b4884dafd1553721cdfae3c0c53032a3a21b9
Wence Cajucom <cajucom.w@gmail.com>
2020-06-06 17:12:29 -0700

    created most of views.py

--- blogForm/blog/views.py
+++ blogForm/blog/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django import forms
+from .models import BlogPost
+
+class PostBlog(forms.Form):
+    username = forms.CharField(max_length=100)
+    text = forms.CharField(widget=forms.Textarea)
 
 def homepage_form(request):
+    print("---Viewed Homepage---")
+    posts = BlogPost.objects.all()
+    if request.method == 'POST':
+        form = PostBlog(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            text = form.cleaned_data['text']
+            BlogPost.objects.create(
+                username=username,
+                text=text,
+            )
+            return redirect('/')
+    else:
+        form = PostBlog()   
     context = {}
     return render(request, 'homepage_form.html', context)
